634163d0107c29df712d95d99d28451bfb1c467c
KSI5 <krissigebs@gmail.com>
2023-08-01 04:02:21 -0700

    fixed 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-
-file_path = ARGV[0]
-if File.exist?(file_path)
-  log_data = File.read(file_path)
-  results = log_data.scan(/(?<=from:|to:|flags:)\+?\w+|\w+/).join(',')
-  puts results
-else
-  puts "Error: Log file not found or inaccessible."
-end
+puts ARGV[0].scan(/(?<=from|to|flags):(\+?\w+|[-?[0-1]:?]+)/).join(',')
