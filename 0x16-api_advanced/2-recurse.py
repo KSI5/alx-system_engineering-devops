e1dbe075311a7e58ea11857b770fc983526f8d59
KSI5 <krissigebs@gmail.com>
2023-11-07 12:22:21 +0000

    Mandatory Task 3

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,34 +7,30 @@ for the given subreddit, the function should return None.
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=""):
     """
     returns a list containing the titles of all hot articles
     for a given subreddit
     """
-    if type(subreddit) is list:
-        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit[0])
-        url = "{}&after={}".format(url, subreddit[1])
-    else:
-        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit)
-        subreddit = [subreddit, ""]
+    url = f"https://api.reddit.com/r/{subreddit}?sort=hot&limit=100&after={after}"
     headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code != 200:
-        return (None)
+        return None
+    
     response = response.json()
-    if "data" in response:
-        data = response.get("data")
-        if not data.get("children"):
-            return (hot_list)
-        for post in data.get("children"):
-            hot_list += [post.get("data").get("title")]
-        if not data.get("after"):
-            return (hot_list)
-        subreddit[1] = data.get("after")
-        recurse(subreddit, hot_list)
-        if hot_list[-1] is None:
-            del hot_list[-1]
-        return (hot_list)
-    else:
-        return (None)
+    data = response.get("data", {})
+
+    if not data.get("children"):
+        return hot_list
+
+    for post in data["children"]:
+        hot_list.append(post["data"]["title"])
+
+    next_page = data.get("after")
+    
+    if next_page:
+        return recurse(subreddit, hot_list, next_page)
+
+    return (hot_list)
